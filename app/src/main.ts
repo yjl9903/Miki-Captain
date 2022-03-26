6e7c08c91c25bea13a3fce454a57501232bb2c07
XLor <yjl9903@vip.qq.com>
2022-03-26 13:24:38 +0800

    fix: mobile view

--- app/src/main.ts
+++ app/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 
 import 'uno.css';
+import '@unocss/reset/normalize.css';
 import './styles/main.css';
 
 import App from './App.vue';
--- app/src/pages/Home.vue
+++ app/src/pages/Home.vue
@@ -6,27 +6,26 @@ import format from 'date-fns/format';
 <template>
   <!-- <h1 m="t-0">{{ up.name }} 的舰团</h1> -->
   <div
-    h="24"
     mt="4"
     px="4"
     py="4"
-    flex
+    md="h-24 flex"
     items="center"
     justify="between"
     rounded
     border-1
     border="light-800"
   >
-    <div flex h="full">
-      <div h="full">
-        <img h="full" rounded="full" class="face" :src="up.face" alt="face" />
+    <div flex class="md:h-full">
+      <div class="md:h-full">
+        <img class="lt-md:h-24 md:h-full" rounded="full" :src="up.face" alt="face" />
       </div>
       <div h="full" ml="2">
         <div flex items="end" h="50%" text="2xl">{{ up.name }}</div>
         <div flex items="start" h="50%" mt="2" text="sm" font="light">{{ up.sign }}</div>
       </div>
     </div>
-    <div>
+    <div lt-md="mt-4">
       <div h="full" text="center">
         <div h="50%">粉丝数</div>
         <div h="50%" mt="2" font="light">{{ up.fans }}</div>
